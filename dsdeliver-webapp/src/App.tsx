c2ef90f5bb1770b1e0e9f0789f14f808bd42dffd
Pablo <pablowoina2205@gmail.com>
2021-01-14 14:03:29 -0300

    Add Navbar

--- dsdeliver-webapp/src/App.tsx
+++ dsdeliver-webapp/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import  { useEffect} from 'react';
 import './App.css';
+import Navbar from './Navbar';
 
 
 function App() {
 
   return (
     <div >
-      <h1>Hello!</h1>
+      <Navbar />
+
     </div>
   );
 }
