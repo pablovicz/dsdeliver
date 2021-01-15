bdf0aedf8450e57b7828b3aa1de896bbdd261afe
Pablo <pablowoina2205@gmail.com>
2021-01-15 11:31:11 -0300

    Add routes and StepsHeader

--- dsdeliver-webapp/src/Home/index.tsx
+++ dsdeliver-webapp/src/Home/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import {ReactComponent as MainImage} from './main.svg';
 import Footer from '../Footer';
+import { Link } from 'react-router-dom';
 
 function Home(){
   return (
@@ -20,9 +21,9 @@ function Home(){
             <br/>
             levaremos na sua porta
           </h3>
-          <a href="orders" className="home-btn-order">
+          <Link to="orders" className="home-btn-order">
             FAZER PEDIDO
-          </a>
+          </Link>
         </div>
         <div className="home-image">
           <MainImage />
