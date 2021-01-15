657fd7d7ee38420b724d4bb93a65fc65fc5e25cc
Pablo <pablowoina2205@gmail.com>
2021-01-15 09:38:49 -0300

    Add footer

--- dsdeliver-webapp/src/Home/index.tsx
+++ dsdeliver-webapp/src/Home/index.tsx
@@ -1,8 +1,10 @@
 import './styles.css';
 import {ReactComponent as MainImage} from './main.svg';
+import Footer from '../Footer';
 
 function Home(){
   return (
+    <>
     <div className="home-container">
       <div className="home-content">
         <div className="home-actions">
@@ -26,8 +28,9 @@ function Home(){
           <MainImage />
         </div>
       </div>
-
     </div>
+    <Footer />
+    </>
 
   );
 }
